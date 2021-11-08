e326716dbf1c7adcf64c6c77d843c6dddd0c6ea1
stvvllrt <75248038+stvvllrt@users.noreply.github.com>
2021-11-08 17:26:44 +0300

    Update stvv.py

--- stvv.py
+++ stvv.py
@@ -1,2 +1,33 @@
-print("hello, test")
-print("testing automated push into github")
+import time
+import RPi.GPIO as GPIO
+
+# Подготавливаем пины GPIO.
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, GPIO.LOW)
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)
+
+# Включаем вращение двигателя 1 в одну сторону.
+GPIO.output(4, GPIO.HIGH)
+
+# ждем 5 секунд.
+time.sleep(5)
+
+# Выключаем двигатель 1.
+GPIO.output(4, GPIO.LOW)
+
+# ждем 10 секунд.
+time.sleep(10)
+
+# Включаем вращение двигателя 1 в другую сторону.
+GPIO.output(17, GPIO.HIGH)
+
+# ждем 5 секунд.
+time.sleep(5)
+
+# Выключаем двигатель 1.
+GPIO.output(17, GPIO.LOW)
