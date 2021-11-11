5cf61fd2accd7cc6b2ad6219e9269d9f8e2ce186
stvvllrt <75248038+stvvllrt@users.noreply.github.com>
2021-11-11 17:09:06 +0300

    Update stvv.py

--- stvv.py
+++ stvv.py
@@ -5,20 +5,20 @@ import RPi.GPIO as GPIO
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT)
-GPIO.output(4, GPIO.LOW)
+GPIO.setup(27, GPIO.OUT)
+GPIO.output(27, GPIO.LOW)
 
 GPIO.setup(17, GPIO.OUT)
 GPIO.output(17, GPIO.LOW)
 
 # Включаем вращение двигателя 1 в одну сторону.
-GPIO.output(4, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
 
 # ждем 5 секунд.
 time.sleep(5)
 
 # Выключаем двигатель 1.
-GPIO.output(4, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
 
 # ждем 10 секунд.
 time.sleep(10)
